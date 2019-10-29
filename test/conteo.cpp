5c23926aeb7d8f19633f2d01de925b608b4e742f
Eric F <efuentesrico@gmail.com>
2019-10-29 21:36:26 +0000

    he agregado el archivo cpp

---
+++ test/conteo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void)
+{
+    int N
+    std::cout<<"Diga el numero de elementos a calcular"<<std::endl;
+    std::cin>>N;
+    
+    int a=0;
+    for(int i=0; i<=N, i++){
+        a+=i;
+        std::cout<<a<<std::endl;
+    }
+    
+    return 0;
+    
