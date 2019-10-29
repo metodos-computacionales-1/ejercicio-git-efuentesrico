42f50a5b93c087bbb26f8715a13393ae6150c427
Eric F <efuentesrico@gmail.com>
2019-10-29 21:48:16 +0000

    area,diametro y perimetro

---
+++ test/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<cmath>
+
+int main(void)
+{
+    float R;
+    float pi = 3,1416
+    std::cout<<"Diga el radio"<<std::endl;
+    std::cin>>R;
+    
+    float d = 2*R
+    float p = 2*pi*R
+    float a = pi*R*R
+        
+    std::cout<<d<<std::endl;
+    std::cout<<p<<std::endl;
+    std::cout<<a<<std::endl;
+    }
+    
+    return 0;
+}
