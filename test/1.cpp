2fd37533e9c987cf13b86dbd6fbf0b455d9d0313
Eric Fuentes <efuentesrico@gmail.com>
2019-10-31 04:39:18 +0000

    Me di cuenta de que tenía un error en el archivo 1 y lo corregi. He terminado.

--- test/1.cpp
+++ test/1.cpp
@@ -4,18 +4,17 @@
 int main(void)
 {
     float R;
-    float pi = 3,1416
+    float pi = 3.1416;
     std::cout<<"Diga el radio"<<std::endl;
     std::cin>>R;
     
-    float d = 2*R
-    float p = 2*pi*R
-    float a = pi*R*R
+    float d = 2*R;
+    float p = 2*pi*R;
+    float a = pi*R*R;
         
-    std::cout<<d<<std::endl;
-    std::cout<<p<<std::endl;
-    std::cout<<a<<std::endl;
-    }
+    std::cout<<"El diametro es "<< d<<std::endl;
+    std::cout<<"El perimetro es "<< p<<std::endl;
+    std::cout<<"El area es "<< a<<std::endl;
     
     return 0;
 }
