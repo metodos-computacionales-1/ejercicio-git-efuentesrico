ef2594bbd2fcbca35a08cf53c8e093c9c210bc70
Eric Fuentes <efuentesrico@gmail.com>
2019-10-31 04:31:37 +0000

    Ya finalice todo el ejercicio WIIII

--- test/2.cpp
+++ test/2.cpp
@@ -8,13 +8,24 @@ int main(void)
     int c;
     float pi = 3.1416;
     
-    std::cout<<"Por favor ingrese el primer numero entero"<<std:endl;
-    std::cin>>a
+    std::cout<<"Por favor ingrese el primer numero entero"<<std::endl;
+    std::cin>>a;
         
-    std::cout<<"Por favor ingrese el segundo numero entero"<<std:endl;
-    std::cin>>b
+    std::cout<<"Por favor ingrese el segundo numero entero"<<std::endl;
+    std::cin>>b;
         
-    std::cout<<"Por favor ingrese el primer numero entero"<<std:endl;
-    std::cin>>c
+    std::cout<<"Por favor ingrese el primer numero entero"<<std::endl;
+    std::cin>>c;
         
-    if((a*a = b*b + c*c) || (b*b = a*a + c*c) || (c*c = b*b + a*a):
+    if((a*a == b*b + c*c) || (b*b == a*a + c*c) || (c*c == b*b + a*a))
+    {
+        std::cout<< "Los lados son compatibles con un triangulo rectangulo."<<std::endl;
+    }
+    else
+    {
+        std::cout<< "Los lados no son compatibles con un triangulo rectangulo."<<std::endl;
+    }
+    
+    return 0;
+    
+}
