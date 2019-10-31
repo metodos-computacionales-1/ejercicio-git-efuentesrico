62f24313f1effb3cd72a17b0ab419d29742b07ad
Eric Fuentes <efuentesrico@gmail.com>
2019-10-31 04:03:55 +0000

    Puse los ingresos de valores y la condicion

--- test/2.cpp
+++ test/2.cpp
@@ -8,5 +8,13 @@ int main(void)
     int c;
     float pi = 3.1416;
     
-    
-    
+    std::cout<<"Por favor ingrese el primer numero entero"<<std:endl;
+    std::cin>>a
+        
+    std::cout<<"Por favor ingrese el segundo numero entero"<<std:endl;
+    std::cin>>b
+        
+    std::cout<<"Por favor ingrese el primer numero entero"<<std:endl;
+    std::cin>>c
+        
+    if((a*a = b*b + c*c) || (b*b = a*a + c*c) || (c*c = b*b + a*a):
